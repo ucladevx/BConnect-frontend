ad0f11466edffffa0fd7285bda21267c273e5a1e
Rahul Natarajan <rahulnatarajan@s-169-232-119-168.resnet.ucla.edu>
2020-03-06 14:32:00 -0800

    connected endpoints and added CORS; pushing straight to master, modified file names because of local errors: changed components to Components, feel free to change back

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -39,6 +39,22 @@ export const login = (username, password) => async (dispatch) => {
     try {
         let response = await axios.post(`${baseUrl}/login`, {username, password})
         console.log(response.data)
+        /*var response = {success: true, data: {
+            user: {name: "Dan Smith", 
+                    job: "Software Engineer",
+                    interests: ["Biking", "Reading", "Boxing"],
+                    friends: ["object_id1", "object_id2"],
+                    bio: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae imperdiet quam. 
+                    Nullam blandit ipsum quis dapibus placerat. Suspendisse mauris eros, 
+                    fringilla at augue in, tempus tincidunt lectus. Phasellus interdum dui a sagittis tempor. 
+                    Ut lorem nisl, faucibus ut dictum at, cursus sit amet diam. Phasellus lectus nulla, blandit 
+                    sit amet imperdiet sit amet, rutrum at nisi. Duis vel sagittis augue, in egestas mi. In ac auctor 
+                    sem. Etiam sit amet magna hendrerit, congue felis et, pharetra risus. Sed ac luctus libero. 
+                    Praesent pretium libero nisl, eget aliquam ex eleifend quis. Curabitur blandit magna sed erat 
+                    dignissim molestie. Maecenas vitae auctor est, non dapibus velit. In hac habitasse platea dictumst.
+                     Maecenas vel diam nec magna convallis dignissim. Nunc egestas maximus dignissim. Ut vel erat dolor.`
+            }
+        }}*/
         if(response.data === "" || response.status !== 200){
             throw new Error("Invalid username or password")
         }
