b3da597f4bf522e5e2522e2a615c44c9c519d9bb
Nopekick <tgkamm@yahoo.com>
2020-05-21 09:41:20 -0700

    create interest selector

--- src/Components/Info/InterestsForm.js
+++ src/Components/Info/InterestsForm.js
@@ -7,37 +7,51 @@ import Button from '@material-ui/core/Button';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 
 function InterestsForm(props) {
-const [interest, changeInterest] = React.useState("");
 const classes = useStyles();
 
-let submitAction = (e) => {
-  e.preventDefault();
-  props.setInterests([...props.interests, interest]); 
-  changeInterest("");
-}
+const interests = { "Food Drink":["Baking", "Cooking", "Trying new foods", "Wine tasting", "Vegan", "Vegetarian"],
+                    "Arts Entertainment":["Reading and writing", "Going to concerts", "Photography",
+                        "Watching Movies", "Drawing and Painting", "Dancing", "Singing", "Playing an instrument"],
+                    "Sports Activites": ["Hiking", "Exploring", "Backpacking", "Camping", "Working out", "Tennis",
+                        "Basketball", "Yoga", "Biking", "Running", "Swimming", "Rock climbing", "Baseball", "Football", "Soccer"],
+                    "Puzzles Games": ["Video games", "Board games", "Trivia", "Chess", "Puzzles", "Escape rooms"]}
+
+const and = "&"
 
   return (
-    <div>
+    <div >
       <Typography className={classes.text} variant="h6" gutterBottom>
         Tell us about your <span style={{color: '#F9D149'}}>interests</span> and  
           <span style={{color: '#F9D149'}}> hobbies</span>.
       </Typography>
-      <div>
-          <form onSubmit={submitAction}>
-            <input value={interest} autoComplete="off" onChange={(e)=>{changeInterest(e.target.value)}} name="interests"
-              placeholder=" Search" className={classes.input}/>
-          </form>
-       
-        <div style={{display: 'flex', margin: '17px'}}>
-         {props.interests.map((interest, i)=>{
-            return  <Chip key={i} className={classes.chip} label={interest}  
-                    variant="outlined" onDelete={()=>{console.log("delete me!")}}/>
-            })}
-        </div>
+      <div style={{display: 'flex', flexDirection: 'column', flexWrap: 'wrap', height: '417px'}}>
+        {Object.keys(interests).map((item, index)=>{
+            return <div key={index} style={{width: '400px'}}>
+                <h2 className={classes.categoryTitle}>{item.split(" ")[0]} <span style={{color: '#F9D149'}}>{and}</span> {item.split(" ")[1]}</h2>
+                  <div style={{display: 'flex', flexWrap: 'wrap'}}>
+                    {interests[item].map((item2, index2)=>{
+                        return <Chip key={index2} className={classes.chip} label={item2}  
+                        variant="outlined" onClick={(e)=>{
+                          if(props.interests.includes(item2)){
+                            props.setInterests(props.interests.filter((it)=>{return it !== item2}))
+                          } else {
+                            props.setInterests([...props.interests, item2]);
+                          }
+                        }} style={props.interests.includes(item2) ? {backgroundColor:"#2d75b0"} : {backgroundColor: "#9FC9EB"}} />
+                    })}
+                  </div>
+            </div>
+        })}
       </div>
     </div>
   );
 }
+
+{/* <div style={{display: 'flex', margin: '17px'}}>
+{props.interests.map((interest, i)=>{
+   return  <Chip key={i} className={classes.chip} label={interest}  
+           variant="outlined" onDelete={()=>{console.log("delete me!")}}/>
+   })} */}
 
 const useStyles = makeStyles(theme => ({ 
   text: {
@@ -51,17 +65,23 @@ const useStyles = makeStyles(theme => ({
     lineHeight: '40px',
   },
   chip: {
-    backgroundColor: '#9FC9EB',
-    padding: '20px',
+    padding: '10px',
     margin: '5px',
     border: 'none',
     color: 'white',
-    fontSize: '17px',
-    fontWeight: '600',
+    fontSize: '14px',
+    fontWeight: '500',
     fontFamily: 'Avenir',
     fontStyle: 'normal',
     borderRadius: '25px',
-
+    transition: "backgroundColor .5",
+  },
+  categoryTitle: {
+    fontFamily: 'Avenir',
+    fontStyle: 'normal',
+    fontWeight: '500',
+    fontSize: '20px',
+    color: 'white'
   },
   input: {
     width: '730px',
